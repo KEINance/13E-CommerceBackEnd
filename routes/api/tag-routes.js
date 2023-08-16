ff8144ff7268022363bb4767215fd44ac3b2915b
KEINance <keinance2@gmail.com>
2023-08-16 17:39:51 -0400

    tag routes get and post

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,10 +16,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [Product, ProductTag],
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
