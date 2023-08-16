728bc124cd4ea0a033dd8142d3950ce166b1c7a3
KEINance <keinance2@gmail.com>
2023-08-16 17:42:24 -0400

    product routes get

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: []
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [],
+    where: {
+      id: req.params.id,
+    }
+  })
+  .then(data => res.status(200).json(data))
+  .catch(err => res.status(400).json(err))
 });
 
 // create new product
